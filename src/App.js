5cfb2bb0e88d2bd6980ac19208f32b7ecabcc43e
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-04-14 21:59:58 +0500

    i removed db.json caller

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import RouterPart from "./parts/routerPart";
-import data from "./db.json";
 
 
 function App() {
-    console.log(data);
     return (
         <div className="app">
             <RouterPart/>
